b1d3a06b686955aa66b3302a515462def12d4773
MarkoAT <no-reply@ixian.io>
2021-02-23 22:36:33 +0100

    fixed issue with sending transactions before the client was synced

--- IxianS2/Meta/Node.cs
+++ IxianS2/Meta/Node.cs
@@ -408,13 +408,9 @@ namespace S2.Meta
 
         public override int getLastBlockVersion()
         {
-            if (tiv.getLastBlockHeader() == null)
+            if (tiv.getLastBlockHeader() == null || tiv.getLastBlockHeader().version < Block.maxVersion)
             {
-                return BlockVer.v6;
-            }
-            if (tiv.getLastBlockHeader().version < BlockVer.v6)
-            {
-                return BlockVer.v6;
+                return Block.maxVersion - 1;
             }
             return tiv.getLastBlockHeader().version;
         }
