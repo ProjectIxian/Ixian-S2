0525324d248aeb03ddd4e5338b847000775d4288
MarkoAT <no-reply@ixian.io>
2024-04-03 21:32:16 +0200

    xs2c-0.5.2 fixed compatibility with new core

--- IxianS2/Meta/Node.cs
+++ IxianS2/Meta/Node.cs
@@ -1,6 +1,7 @@
 ﻿using IXICore;
 using IXICore.Meta;
 using IXICore.Network;
+using IXICore.RegNames;
 using IXICore.Utils;
 using S2.Network;
 using System;
@@ -592,6 +593,32 @@ namespace S2.Meta
         {
             // TODO TODO implement this properly
             return ConsensusConfig.minBlockSignerPowDifficulty;
+        }
+
+        public override byte[] getBlockHash(ulong blockNum)
+        {
+            Block b = getBlockHeader(blockNum);
+            if (b == null)
+            {
+                return null;
+            }
+
+            return b.blockChecksum;
+        }
+
+        public override byte[] calculateRegNameChecksumFromUpdatedDataRecords(byte[] name, List<RegisteredNameDataRecord> dataRecords, ulong sequence, Address nextPkHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] calculateRegNameChecksumForRecovery(byte[] name, Address recoveryHash, ulong sequence, Address nextPkHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override RegisteredNameRecord getRegName(byte[] name, bool useAbsoluteId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
